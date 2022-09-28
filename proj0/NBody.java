54b7553261037a7743222a1b02ffaac789dde66f
minjnlgc <111153630+minjnlgc@users.noreply.github.com>
2022-09-28 22:01:32 +0100

    Getting Started with the Simulator

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,32 @@
 public class NBody {
+    public static double readRadius(String fileName){
+        In in = new In(fileName);
+        in.readInt();
+        return in.readDouble();
+    }
+
+    public static Planet[] readPlanets(String fileName){
+        In in = new In(fileName);
+        Planet[] planets = new Planet[in.readInt()];
+        in.readDouble();
+        for(int i=0; i < planets.length; i++){
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return planets;
+    }
     
+
+    public static void main(String[] args){
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double radius = readRadius(filename);
+        Planet[] planets = readPlanets(filename);
+    }
 }
