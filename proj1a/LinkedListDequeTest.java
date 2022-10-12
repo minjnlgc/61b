a07bc0da2ca2e8f73cc5831a8d85fa9cf972a182
minjnlgc <111153630+minjnlgc@users.noreply.github.com>
2022-10-12 17:10:31 +0100

    fixed bug

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -87,18 +87,6 @@ public class LinkedListDequeTest {
 	public static void main(String[] args) {
 		System.out.println("Running tests.\n");
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		lld1.addFirst(3);
-		lld1.addLast(4);
-		lld1.removeLast();
-		lld1.removeLast();
-		lld1.addFirst(3);
-		lld1.addLast(4);
-		lld1.removeLast();
-		lld1.removeLast();
-		lld1.addFirst(7);
-		lld1.removeLast();
-		lld1.addLast(10);
-		lld1.removeFirst();
 		lld1.addFirst(12);
 		lld1.addLast(13);
 		System.out.println((lld1.getRecursive(1)));
