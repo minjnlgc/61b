a37d27fbb6227ce74314112920d4d4137eccf012
minjnlgc <111153630+minjnlgc@users.noreply.github.com>
2022-10-24 00:33:20 +0100

    try2.0

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,166 +1,171 @@
+/** second part of project1A.
+ * deque implemented by array
+ * @author FlyingPig
+ */
 public class ArrayDeque<T> {
-    private T[] items;
+
+    /** array to save data.*/
+    private T[] array;
+    /** size of the deque. */
     private int size;
-    private int nextFirst;
-    private int nextLast;
 
-    /**
-     * Creates an empty array deque
-     */
+    /** size of the array. */
+    private int length;
+
+    /** front index. */
+    private int front;
+
+    /** last index. */
+    private int last;
+
+    /** constructor for ArrayDeque. */
     public ArrayDeque() {
-        items = (T[]) new Object[8];
+        array = (T[]) new Object[8];
         size = 0;
-        nextFirst = 3;
-        nextLast = 4;
+        length = 8;
+        front = 4;
+        last = 4;
     }
 
-    /**
-     * Adds an item of type T to the front of the deque.
-     */
-    public void addFirst(T item) {
-        items[nextFirst] = item;
-
-        size++;
-        nextFirst--;
-        if (nextFirst == -1) {
-            nextFirst = items.length - 1;
-        }
-        resize();
-    }
-
-    /**
-     * Adds an item of type T to the back of the deque.
-     */
-    public void addLast(T item) {
-        items[nextLast] = item;
-
-        size++;
-        nextLast++;
-        if (nextLast == items.length) {
-            nextLast = 0;
-        }
-        resize();
-    }
-
-    /**
-     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
-     */
-    public T removeFirst() {
-        T toRemove = get(0);
-        if (nextFirst == items.length - 1) {
-            items[0] = null;
-        } else {
-            items[nextFirst + 1] = null;
-        }
-
-        if (size > 0) {
-            size--;
-        }
-        nextFirst++;
-        if (nextFirst == items.length) {
-            nextFirst = 0;
-        }
-        resize();
-        return toRemove;
-    }
-
-    /**
-     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
-     */
-    public T removeLast() {
-        T toRemove = get(size - 1);
-        if (nextLast == 0) {
-            items[items.length - 1] = null;
-        } else {
-            items[nextLast - 1] = null;
-        }
-
-        if (size > 0) {
-            size--;
-        }
-        nextLast--;
-        if (nextLast == -1) {
-            nextLast = items.length - 1;
-        }
-        resize();
-        return toRemove;
-    }
-
-    /**
-     * Returns true if deque is empty, false otherwise.
+    /** decide if the deque is empty.
+     * @return true if the deque is empty, vice versa.
      */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /**
-     * Returns the number of items in the deque.
-     */
+    /** return the size of the deque. */
     public int size() {
         return size;
     }
 
-    /**
-     * Prints the items in the deque from first to last, separated by a space.
+    /** return the "index - 1".
+     * @param index index
      */
-    public void printDeque() {
-        for (int i = 0; i < items.length; i++) {
-            System.out.print(items[i] + " ");
+    private int minusOne(int index) {
+        if (index == 0) {
+            return length - 1;
         }
+        return index - 1;
     }
 
-    /**
-     * Gets the item at the given index, where 0 is the front, 1 is the next item,
-     * and so forth. If no such item exists, returns null. Must not alter the deque!
+    /** return the "index + 1".
+     * @param index index
+     */
+    private int plusOne(int index, int module) {
+        index %= module;
+        if (index == module - 1) {
+            return 0;
+        }
+        return index + 1;
+    }
+
+    private void grow() {
+        T[] newArray = (T[]) new Object[length * 2];
+        int ptr1 = front;
+        int ptr2 = length;
+        while (ptr1 != last) {
+            newArray[ptr2] = array[ptr1];
+            ptr1 = plusOne(ptr1, length);
+            ptr2 = plusOne(ptr2, length * 2);
+        }
+        front = length;
+        last = ptr2;
+        array = newArray;
+        length *= 2;
+    }
+
+    private void shrink() {
+        T[] newArray = (T[]) new Object[length / 2];
+        int ptr1 = front;
+        int ptr2 = length / 4;
+        while (ptr1 != last) {
+            newArray[ptr2] = array[ptr1];
+            ptr1 = plusOne(ptr1, length);
+            ptr2 = plusOne(ptr2, length / 2);
+        }
+        front = length / 4;
+        last = ptr2;
+        array = newArray;
+        length /= 2;
+    }
+
+    /** add one item at the front of the deque.
+     * @param item the item we want to add
+     */
+    public void addFirst(T item) {
+        if (size == length - 1) {
+            grow();
+        }
+        front = minusOne(front);
+        array[front] = item;
+        size++;
+    }
+
+    /** add one item at the end of the deque.
+     * @param item item we want to add
+     */
+    public void addLast(T item) {
+        if (size == length - 1) {
+            grow();
+        }
+        array[last] = item;
+        last = plusOne(last, length);
+        size++;
+    }
+
+    /** remove the first item.
+     * @return the removed first item
+     */
+    public T removeFirst() {
+        if (length >= 16 && length / size >= 4) {
+            shrink();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T ret = array[front];
+        front = plusOne(front, length);
+        size--;
+        return ret;
+    }
+
+    /** remove the last item.
+     * @return the removed last item
+     */
+    public T removeLast() {
+        if (length >= 16 && length / size >= 4) {
+            shrink();
+        }
+        if (size == 0) {
+            return null;
+        }
+        last = minusOne(last);
+        size--;
+        return array[last];
+    }
+
+    /** return the item indexed at index.
+     * @param index index
      */
     public T get(int index) {
         if (index >= size) {
             return null;
         }
-        int mod = (nextFirst + 1 + index) % items.length;
-        return items[mod];
+        int ptr = front;
+        for (int i = 0; i < index; i++) {
+            ptr = plusOne(ptr, length);
+        }
+        return array[ptr];
     }
 
-    /**
-     * get the load factor of an deque
-     */
-    private double getLoadFactor() {
-        return ((double) size) / items.length;
-    }
-
-    /**
-     * resize the array
-     **/
-    private void resize() {
-        boolean flag = false;
-        T[] arr = (T[]) new Object[items.length * 2];
-        if (getLoadFactor() > 0.75) {
-            flag = true;
-        }
-        if (items.length >= 16) {
-            if (getLoadFactor() < 0.25) {
-                arr = (T[]) new Object[items.length / 2];
-                flag = true;
-            }
-        }
-        if (flag) {
-            if (items[0] == null && items[items.length - 1] == null) {
-                System.arraycopy(items, nextFirst + 1, arr, 0, size);
-            } else {
-                System.arraycopy(items, nextFirst + 1, arr, 0, items.length - nextFirst - 1);
-                int len = 0;
-                for (int i = 0; i < arr.length; i++) {
-                    if (arr[i] == null) {
-                        break;
-                    }
-                    len++;
-                }
-                System.arraycopy(items, 0, arr, len, nextLast);
-            }
-            items = arr;
-
-            nextFirst = arr.length - 1;
-            nextLast = size;
+    /** print the entire deque from front to end. */
+    public void printDeque() {
+        int ptr = front;
+        while (ptr != last) {
+            System.out.print(array[ptr] + " ");
+            ptr = plusOne(ptr, length);
         }
     }
+
 }
