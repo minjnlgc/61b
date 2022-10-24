67ac0c34663e2eb427e79b2f6e38b31cd77489b3
minjnlgc <111153630+minjnlgc@users.noreply.github.com>
2022-10-24 14:00:06 +0100

    create random integers

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class TestArrayDequeGold {
+    static StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+    static ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+
+    @Test
+    public void testArrayDeque() {
+        Integer num1 = StdRandom.uniform(1, 100);
+        Integer num2 = StdRandom.uniform(1, 100);
+        Integer num3 = StdRandom.uniform(1, 100);
+        Integer num4 = StdRandom.uniform(1, 100);
+        Integer num5 = StdRandom.uniform(1, 100);
+        Integer num6 = StdRandom.uniform(1, 100);
+        Integer num7 = StdRandom.uniform(1, 100);
+        Integer num8 = StdRandom.uniform(1, 100);
+
+    }
+}
