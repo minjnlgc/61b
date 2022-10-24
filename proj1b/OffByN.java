468581cca05e6ac8fd12f66260e59e43e1ae02fc
minjnlgc <111153630+minjnlgc@users.noreply.github.com>
2022-10-24 02:08:47 +0100

    Task 5

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,2 +1,11 @@
-public class OffByN {
+public class OffByN implements CharacterComparator {
+    private int n;
+    public OffByN(int N) {
+        this.n = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        return Math.abs(x - y) == this.n;
+    }
 }
